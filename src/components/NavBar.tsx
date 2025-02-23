0372f888b8ff9cc8a3c93465585a6a95d6907a52
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-02-23 12:55:21 -0300

    Delete ui folder, implement color mode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,16 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
     return (
-        <HStack>
-            <Image src={logo} boxSize={"60px"} margin={"0.5rem"} />
-            <Text>NavBar</Text>
+        <HStack
+            justifyContent={"space-between"}
+            paddingBlock={"10px"}
+            paddingInline={"15px"}
+        >
+            <Image src={logo} boxSize={"60px"} />
+            <ColorModeSwitch />
         </HStack>
     );
 };
