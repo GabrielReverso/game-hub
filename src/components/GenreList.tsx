d689bada7252cdd420fdf58a0b6ad21dd1927a1f
GVerso <112283257+GabrielReverso@users.noreply.github.com>
2025-02-23 17:40:12 -0300

    Filter by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,13 @@
-import useGenres from "@/hooks/useGenres";
+import useGenres, { Genre } from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { Button, HStack, Image, List, ListItem } from "@chakra-ui/react";
 import GenreListSkeleton from "./GenreListSkeleton";
 
-const GenreList = () => {
+interface Props {
+    onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
     const { data: genres, isLoading, error } = useGenres();
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
@@ -25,7 +29,13 @@ const GenreList = () => {
                             borderRadius={"8px"}
                             src={getCroppedImageUrl(genre.image_background)}
                         />
-                        <Text fontSize={"lg"}>{genre.name}</Text>
+                        <Button
+                            onClick={() => onSelectGenre(genre)}
+                            fontSize={"lg"}
+                            variant={"link"}
+                        >
+                            {genre.name}
+                        </Button>
                     </HStack>
                 </ListItem>
             ))}
